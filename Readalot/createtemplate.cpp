3baac7e21d5e638a21cf735beea43b9d51a3ddcc
Pavel Sarlov <sarlovpavel@gmail.com>
2022-03-19 14:25:55 +0200

    remove spaghetti

--- Readalot/createtemplate.cpp
+++ Readalot/createtemplate.cpp
@@ -53,36 +53,13 @@ QJsonObject createTemplate::getCurrentObject()
 {
     QJsonObject currentObj;
 
+    QList<QCheckBox*> checkboxes = this->findChildren<QCheckBox*>(QRegularExpression(".*"));
+    for (auto it = checkboxes.begin(); it != checkboxes.end(); it++)
+    {
+        currentObj.insert((*it)->objectName().remove(0, 8).toLower(), (*it)->isChecked());
+    }
+
     currentObj.insert("name", ui->lineEditNameTemplate->text());
-    currentObj.insert("nameBook", ui->checkBoxName->isChecked());
-    currentObj.insert("author", ui->checkBoxAuthor->isChecked());
-    currentObj.insert("series", ui->checkBoxSeries->isChecked());
-    currentObj.insert("dateStarted", ui->checkBoxDateStarted->isChecked());
-    currentObj.insert("dateFinished", ui->checkBoxDateFinished->isChecked());
-    currentObj.insert("score", ui->checkBoxScore->isChecked());
-    currentObj.insert("publisher", ui->checkBoxPublisher->isChecked());
-    currentObj.insert("datePublished", ui->checkBoxDatePublished->isChecked());
-    currentObj.insert("datePublishedBulgaria", ui->checkBoxDatePublishedBulgaria->isChecked());
-    currentObj.insert("pages", ui->checkBoxPages->isChecked());
-    currentObj.insert("format", ui->checkBoxFormat->isChecked());
-    currentObj.insert("ficnonfic", ui->checkBoxFiction->isChecked());
-    currentObj.insert("genre", ui->checkBoxGenre->isChecked());
-    currentObj.insert("source", ui->checkBoxSource->isChecked());
-    currentObj.insert("translation", ui->checkBoxTranslation->isChecked());
-    currentObj.insert("language", ui->checkBoxLanguage->isChecked());
-    currentObj.insert("country", ui->checkBoxCountry->isChecked());
-    currentObj.insert("notes", ui->checkBoxNotes->isChecked());
-    currentObj.insert("spicy", ui->checkBoxSpicy->isChecked());
-    currentObj.insert("tropes", ui->checkBoxTropes->isChecked());
-    currentObj.insert("fantasyKind", ui->checkBoxFantasyKind->isChecked());
-    currentObj.insert("magicSystem", ui->checkBoxMagicSystem->isChecked());
-    currentObj.insert("scifiKind", ui->checkBoxScifiKind->isChecked());
-    currentObj.insert("future", ui->checkBoxFuture->isChecked());
-    currentObj.insert("mystery", ui->checkBoxMystery->isChecked());
-    currentObj.insert("scary", ui->checkBoxScariness->isChecked());
-    currentObj.insert("disgust", ui->checkBoxDisgust->isChecked());
-    currentObj.insert("epoch", ui->checkBoxEpoch->isChecked());
-    currentObj.insert("correctness", ui->checkBoxCorrectness->isChecked());
 
     return currentObj;
 }
@@ -116,37 +93,14 @@ void createTemplate::saveToFile()
 void createTemplate::setTemplateVars()
 {
     QJsonValue currentObj = this->templates.at(this->currentTemplateIndex);
-    ui->lineEditNameTemplate->setText(currentObj["name"].toString());
-    ui->checkBoxName->setChecked(currentObj["nameBook"].toBool());
-    ui->checkBoxAuthor->setChecked(currentObj["author"].toBool());
-    ui->checkBoxSeries->setChecked(currentObj["series"].toBool());
-    ui->checkBoxDateStarted->setChecked(currentObj["dateStarted"].toBool());
-    ui->checkBoxDateFinished->setChecked(currentObj["dateFinished"].toBool());
-    ui->checkBoxScore->setChecked(currentObj["score"].toBool());
-    ui->checkBoxPublisher->setChecked(currentObj["publisher"].toBool());
-    ui->checkBoxDatePublished->setChecked(currentObj["datePublished"].toBool());
-    ui->checkBoxDatePublishedBulgaria->setChecked(currentObj["datePublishedBulgaria"].toBool());
-    ui->checkBoxPages->setChecked(currentObj["pages"].toBool());
-    ui->checkBoxFormat->setChecked(currentObj["format"].toBool());
-    ui->checkBoxFiction->setChecked(currentObj["ficnonfic"].toBool());
-    ui->checkBoxGenre->setChecked(currentObj["genre"].toBool());
-    ui->checkBoxSource->setChecked(currentObj["source"].toBool());
-    ui->checkBoxTranslation->setChecked(currentObj["translation"].toBool());
-    ui->checkBoxLanguage->setChecked(currentObj["language"].toBool());
-    ui->checkBoxCountry->setChecked(currentObj["country"].toBool());
-    ui->checkBoxNotes->setChecked(currentObj["notes"].toBool());
-    ui->checkBoxSpicy->setChecked(currentObj["spicy"].toBool());
-    ui->checkBoxTropes->setChecked(currentObj["tropes"].toBool());
-    ui->checkBoxFantasyKind->setChecked(currentObj["fantasyKind"].toBool());
-    ui->checkBoxMagicSystem->setChecked(currentObj["magicSystem"].toBool());
-    ui->checkBoxScifiKind->setChecked(currentObj["scifiKind"].toBool());
-    ui->checkBoxFuture->setChecked(currentObj["future"].toBool());
-    ui->checkBoxMystery->setChecked(currentObj["mystery"].toBool());
-    ui->checkBoxScariness->setChecked(currentObj["scary"].toBool());
-    ui->checkBoxDisgust->setChecked(currentObj["disgust"].toBool());
-    ui->checkBoxEpoch->setChecked(currentObj["epoch"].toBool());
-    ui->checkBoxCorrectness->setChecked(currentObj["correctness"].toBool());
-}
+
+    QList<QCheckBox*> checkboxes = this->findChildren<QCheckBox*>(QRegularExpression(".*"));
+    for (auto it = checkboxes.begin(); it != checkboxes.end(); it++)
+    {
+        (*it)->setChecked(currentObj[(*it)->objectName().remove(0, 8).toLower()].toBool());
+    }
+
+    ui->lineEditNameTemplate->setText(currentObj["name"].toString());}
 
 void createTemplate::on_pushButtonSave_clicked()
 {
